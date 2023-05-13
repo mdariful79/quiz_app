3c8eed603eda9efc2036cc084e9cf5605829ea97
mdariful79 <mdariful057@gmail.com>
2023-05-13 14:07:22 +0600

    result&resultTable&resultCss added

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -12,11 +12,11 @@ export default function Main() {
   return (
     <div className='container'>
         <h1 className='title text-light'>Quiz Application</h1>
-
+        
         <ol>
             <li>You will be asked 10 questions one after another.</li>
             <li>10 points is awarded for the correct answer.</li>
-            <li>Each question has three options. You can choose only one options.</li>
+            <li>Each question has 4 options. You can choose only one options.</li>
             <li>You can review and change answers before the quiz finish.</li>
             <li>The result will be declared at the end of the quiz.</li>
         </ol>
---
+++ client/src/database/data.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        question : "Who invented C++?",
+        options : [
+            'Dennis Ritchie',
+            'Ken Thompson',
+            'Brian Kernighan',
+            ' Bjarne Stroustrup',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which of the following is correct about this pointer in C++?",
+        options : [
+            'document.this pointer is passed as a hidden argument in all static variables of a class()',
+            'this pointer is passed as a hidden argument in all the functions of a class',
+            'this pointer is passed as a hidden argument in all non-static functions of a class',
+            'this pointer is passed as a hidden argument in all static functions of a class',
+        ]
+    },
+    {
+        id: 3,
+        question : "Which of the following type is provided by C++ but not C?",
+        options : [
+            'double',
+            'float',
+            'int',
+            'int',
+        ]
+    },
+    {
+        id: 4,
+        question : "What does the toString() method returnBy default, all the files in C++ are opened in _________ mode?",
+        options : [
+            'Binary',
+            'VTC',
+            'Text',
+            'ISCII',
+        ]
+    },
+    {
+        id: 5,
+        question : "What is the size of wchar_t in C++?",
+        options : [
+            'Based on the number of bits in the system',
+            '2 or 4',
+            '4',
+            '2',
+        ]
+    }
+]
