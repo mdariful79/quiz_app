fe1592b46bdba0ed4bdd810e41b2bdf2ad9db25f
mdariful79 <mdariful057@gmail.com>
2023-05-14 11:28:46 +0600

    Hooks&Redux added and moving Question

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Questions from './Questions'
 
+import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestion';
 
+/** redux store import */
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function Quiz() {
 
+    // const trace = useSelector(state => state.questions.trace);
+    const { queue, trace } = useSelector(state => state.questions);
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+        console.log(trace)
+    })
+
+    /** next button event handler */
     function onNext(){
         console.log('On next click')
+
+        if(trace < queue.length){
+            /** increase the trace value by one using MoveNextAction */
+            dispatch(MoveNextQuestion());
+        }
+        
     }
 
+    /** Prev button event handler */
     function onPrev(){
         console.log('On onPrev click')
+        if(trace > 0){
+            /** decrease the trace value by one using MovePrevQuestion */
+            dispatch(MovePrevQuestion());
+        }
     }
 
   return (
@@ -20,6 +42,7 @@ export default function Quiz() {
 
         {/* display questions */}
         <Questions />
+
         <div className='grid'>
             <button className='btn prev' onClick={onPrev}>Prev</button>
             <button className='btn next' onClick={onNext}>Next</button>
